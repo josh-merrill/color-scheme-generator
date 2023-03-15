e39984d1abf7d8e73e0533d8b9851f3b9b2782ad
Josh Merrill <joshmmerrill@outlook.com>
2023-03-15 11:02:38 +0000

    REFACTOR: Small code adjustment

--- utils.js
+++ utils.js
@@ -1,22 +1,18 @@
-
 // Add click event listeners to each "generate-hex" div
 function handleHexClicks() {
-  const generateHexDivs = document.getElementsByClassName('generate-hex')
+  const generateHexDivs = document.getElementsByClassName("generate-hex");
   for (let generateHexDiv of generateHexDivs) {
-    generateHexDiv.addEventListener('click', () => {
-      const hexValue = generateHexDiv.textContent
+    generateHexDiv.addEventListener("click", () => {
+      const hexValue = generateHexDiv.textContent;
 
-        // Copy the text inside the text field
-        navigator.clipboard.writeText(hexValue);
+      // Copy the text inside the text field
+      navigator.clipboard.writeText(hexValue);
 
-        // Insert the copied text to the page
-        document.getElementById('selection-instruction').innerHTML = `
-        <small>HEX code ${hexValue} was copied to your clipboard</small>`
-      })
+      // Insert the copied text to the page
+      document.getElementById("selection-instruction").innerHTML = `
+        <small>HEX code ${hexValue} was copied to your clipboard</small>`;
+    });
   }
 }
 
-
-
-
-export {handleHexClicks}
+export { handleHexClicks };
